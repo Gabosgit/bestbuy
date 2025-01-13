b6c64f3b5ca37ad97d21e5ca7010e91a66aa4a85
Gabo <no@email>
2025-01-13 17:30:12 +0100

    The application has been tested and docstrings have been added.

--- products.py
+++ products.py
@@ -15,33 +15,45 @@ class Product:
             raise Exception("The quantity can't be negative")
 
     def get_quantity(self):
+        """ Return the quantity of a product """
         return self.quantity
 
     def set_quantity(self, quantity):
+        """ Sets the available quantity of a product
+            If the Quantity is 0, it deactivates the product
+        """
         self.quantity = quantity
         if quantity == 0:
-            self.active = False
+            self.deactivate()
 
     def is_active(self):
         return self.active
 
     def activate(self):
+        """ Activate the product """
         self.active = True
 
     def deactivate(self):
+        """ Deactivate the product """
         self.active = False
 
     def show(self):
+        """ Returns a string with information about the attributes of a product """
         return f"{self.name}, Price: {self.price}, Quantity: {self.quantity}"
 
     def buy(self, buy_quantity):
+        """ Returns the total price for a given quantity of a product
+            Validates the given quantity, which has to be a positive number.
+            If there is not enough in stock, a warning is printed.
+        """
         if buy_quantity > self.quantity:
-            raise Exception("Not enough Quantity in stock")
-
+            print("Error while making order! Quantity larger than what exists")
+            #raise Exception("Not enough Quantity in stock")
         if buy_quantity < 1:
             raise Exception("Quantity must be a positive number and at least 1")
 
         total_price = self.price * buy_quantity
-        self.quantity = self.quantity - buy_quantity
+        self.quantity -= buy_quantity
+        self.set_quantity(self.quantity)
 
         return total_price
--- store.py
+++ store.py
@@ -1,67 +1,61 @@
 import products
 
 class Store:
+    """ Creates a store by starting it with a list of products """
     def __init__(self, products_list):
         self.products_list = products_list
 
+
     def add_product(self, product):
+        """ Allows to add a product to the product list """
         self.products_list.append(product)
 
+
     def remove_product(self, product):
+        """ Allows to remove a product from the product list """
         self.products_list.remove(product)
 
+
     def get_total_quantity(self):
-        return len(self.products_list)
+        """ Prints the total of products in the store """
+        total_quantity = 0
+        for product in self.products_list:
+            total_quantity += product.quantity
+        print(f"\nTotal of {total_quantity} items in store\n")
+
 
     def get_all_products(self):
+        """
+            Check if the product is active.
+            If it is not active it removes it from the products list to not show it and not count it as a purchase option.
+            Prints a numbered list of products
+        """
+        print()
         for product in self.products_list:
-            if product.is_active():
-                print(product.show())
+            if not product.is_active():
+                self.remove_product(product)
+
+        for index, product in enumerate(self.products_list):
+            if not product.is_active():
+                self.remove_product(product)
+            elif product.is_active():
+                print(f"{index+1}. {product.show()}")
+        print()
+
 
     def order(self, shopping_list):
-        self.products_list = shopping_list
+        """ Gets a list of tuples from the user. Each tuple contains the product index and the quantity of this product to order.
+            Returns the total price for the entire purchase
+        """
         total_order = 0
-
         for product, quantity in shopping_list:
             total_product_price = product.buy(quantity) # Product price * quantity
             total_order = total_order + total_product_price # Result of all products purchased
-
         return total_order
 
 
 def main():
-    bose = products.Product("Bose QuietComfort Earbuds", price=250, quantity=500)
-    mac = products.Product("MacBook Air M2", price=1450, quantity=100)
-
-    store = Store([bose, mac])
-    print(store.products_list)
-
-    print()
-
-    pixel = products.Product("Google Pixel 7", price=500, quantity=250)
-    store.add_product(pixel)
-
-    print()
-    print(store.products_list)
-
-    print()
-
-    store.remove_product(bose)
-    print(store.products_list)
-
-    print(store.get_total_quantity())
-
-    #mac.deactivate()
-    store.add_product(bose)
-
-    print()
-    store.get_all_products()
-    print()
-
-    price = store.order([(bose, 5), (mac, 30), (bose, 10)])
-    print(f"Order cost: {price} dollars.")
-
-
+    pass
 
 
 if __name__ == '__main__':
